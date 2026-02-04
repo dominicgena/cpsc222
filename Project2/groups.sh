1dcc768e76ee5b9f44d078117cfd79bde90017fc
dominicgena <dominic.gena2@gmail.com>
2026-02-04 11:36:41 -0500

    2/4/2026 - Added arguments to shell scripts. Scripts must now be run as

--- Project2/groups.sh
+++ Project2/groups.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -s -d "username=test&password=abcABC123" http://127.0.0.1:5000/api && \curl -s http://127.0.0.1:5000/api/groups | tr -d '\n\r' | grep -oP '(?<=<h1>)[^<]+'
+curl -s -d "username=$1&password=$2" http://127.0.0.1:5000/api && \curl -s http://127.0.0.1:5000/api/groups | tr -d '\n\r' | grep -oP '(?<=<h1>)[^<]+'
